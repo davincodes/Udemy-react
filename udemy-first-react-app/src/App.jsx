763e3e3d1a7c421a9c86a66fc248a42cb4551c61
davincodes <aarvinantonio@gmail.com>
2024-02-29 22:51:47 +0800

    props

---
+++ udemy-first-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Chicken from './Chicken'
+import Greeter from './Greeter'
+
+function App() {
+  return <div>
+      <Greeter person="bill"/>
+      <Greeter person="Joe"/>
+      <Greeter person="Don"/>
+  </div>
+}
+
+export default App
---
+++ udemy-first-react-app/src/Greeter.jsx
@@ -0,0 +1,5 @@
+function Greeter({ person }){
+    return <h1>Hi there, {person}!!!</h1>
+}
+
+export default Greeter;
